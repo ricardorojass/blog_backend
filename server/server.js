87dafcad767be0cabc7cf68ca61fddb59d2c1dc7
Ricardo Rojas <reddrum7@gmail.com>
2019-02-28 22:01:40 -0500

    [Fix] Remove user require

--- server/server.js
+++ server/server.js
@@ -7,7 +7,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const {mongose} = require('./config/mongoose');
-const {User} = require('./models/user.js');
 const {Story} = require('./models/story.js');
 
 app.use(morgan('dev'));

006c0054be854a84ffa16f95164f2ce7c5ddc9a2
Ricardo Rojas <reddrum7@gmail.com>
2019-03-04 18:13:38 -0500

    [Feature] Update Story resource using Async Await way

--- server/server.js
+++ server/server.js
@@ -14,68 +14,57 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // GET/stories
-app.get('/stories', (req, res) => {
-  Story.find().then((stories) => {
+app.get('/stories', async (req, res) => {
+  try {
+    const stories = await Story.find({})
     res.send({stories});
-  }, (e) => {
-    res.status(400).send(e);
-  });
+  } catch (e) {
+    res.status(400).send(e)
+  }
 });
 
-app.get('/stories/:title', (req, res) => {
+app.get('/stories/:title', async (req, res) => {
   let title = req.params.title
+  try {
+    const story = await Story.findOne({title: title});
 
-  Story.findOne({title: title}).then(story => {
-    if(!story)
-      res.status(404).send({});
-
-    res.send({story})
-    })
-    .catch(err => {
-      res.status(400).send({});
-    });
+    if (!story) {
+      return res.status(404).send()
+    }
+    res.send(story);
+  } catch (e) {
+    res.status(500).send();
+  }
 })
 
-app.get('/:title/edit', (req, res) => {
+app.get('/:title/edit', async (req, res) => {
   let title = req.params.title
   if (!title)
     return res.status(400).send('Missing URL parameter')
 
-  Story.findOne({
-    title: title
-  })
-    .then(story => {
-      if (!story)
-        return res.status(404).send({});
+  try {
+    const story = await Story.findOne({title: title});
 
-      console.log('edit', story);
-      res.status(200).send(story);
-    })
-    .catch(err => {
-      res.status(500).json(err)
-    })
+    if (!story) {
+      return res.status(404).send()
+    }
+    res.send(story);
+  } catch (e) {
+    res.status(500).send();
+  }
 })
 
 // POST/stories
-app.post('/stories', (req, res) => {
-  const storyData = req.body
+app.post('/stories', async (req, res) => {
+  const story = new Story(req.body);
 
-  let model = new Story({
-    title: storyData.title,
-    body: storyData.body,
-  });
-
-  model.save().then(doc => {
-    if (!doc || doc.length === 0) {
-      return res.status(500).send(doc)
-    }
-
-    res.status(200).json(doc)
-  })
-  .catch(err => {
-    res.status(400).json(err)
-  })
-})
+  try {
+    await story.save()
+    res.status(201).send(story);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);

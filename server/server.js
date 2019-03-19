2c7cede1d50c719a2ef87297393028327efe5b37
Ricardo Rojas <reddrum7@gmail.com>
2019-03-19 14:44:38 -0500

    [Build] Add Cool ascii faces module

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
-require('./config/mongoose');
+require('./config/mongoose')
 
 // Without middleware: new request -> run route handler
 
@@ -15,15 +16,15 @@ require('./config/mongoose');
 //   res.status(503).send('We are in maintenance!');
 // });
 
-app.use(morgan('dev'));
-app.use(cors());
-app.use(express.json());
-app.use(require('./routes'));
+app.use(morgan('dev'))
+app.use(cors())
+app.use(express.json())
+app.use(require('./routes'))
 
 
 
 app.listen(port, () => {
-  console.log(`Started on port ${port}`);
-});
+  console.log(`Started on port ${port}`)
+})
 
-module.exports = {app};
+module.exports = {app}
